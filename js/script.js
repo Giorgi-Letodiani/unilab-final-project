4097d3a9965d5afe8f529e0a97f1461de4006a25
Giorgi-Letodiani <giorgi.letodiani.1999@gmail.com>
2021-08-24 05:18:25 +0400

    add slick slider

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ let burggerButton = document.getElementById('burger-menu');
 burggerButton.addEventListener('click', function(){
     navigationNav.classList.toggle("active");
 });
+$(function(){
+    $('.single-item').slick({
+        arrows: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
