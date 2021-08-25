cb0fe306c7dd5075701e2a08df02fec4745fb475
Giorgi-Letodiani <giorgi.letodiani.1999@gmail.com>
2021-08-25 05:06:15 +0400

    add section, responsive, delete slick slider

--- js/script.js
+++ js/script.js
@@ -4,13 +4,3 @@ let burggerButton = document.getElementById('burger-menu');
 burggerButton.addEventListener('click', function(){
     navigationNav.classList.toggle("active");
 });
-$(function(){
-    $('.single-item').slick({
-        arrows: true,
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-});
